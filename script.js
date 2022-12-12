c92c70a340add26446752ab68257004c3ae2c1e5
Darius <darius.draksas244@gmail.com>
2022-12-12 21:17:27 +0200

    Added display score function

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ import questions from './data.js'
 
 
 console.log(questions)
+
+function displayEndScore(score) {
+    let output = 'Invalid score'
+
+    if (score <= 5) {
+        output = 'Very bad'
+    }
+    else if (score <= 9) {
+        output = 'bad'
+    }
+    else if (score <= 15) {
+        output = 'Pretty good'
+    }
+    else if (score <= 19) {
+        output = 'Good'
+    }
+    else {
+        output = 'Perfect'
+    }
+
+    console.log(output)
+    
+    //To do: display as div
+}
+
