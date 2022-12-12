6d6b98992f34e567f115a246c0408cce2e2487af
Moksleivis <Moksleivis@kitm.local>
2022-12-12 15:25:10 +0200

    first commit nothing cool

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 import questions from './data.js'
 
+const mainDiv = document.querySelector("#main")
 
-console.log(questions)
+
+// function print_questions_number(){
+//     for(let i in questions){
+//         const my_p = document.createElement("p")
+//         my_p.innerText = `Klausimas ${questions[i].id} iš ${questions.length}`
+//         mainDiv.appendChild(my_p)
+//     }    
+// }
+
+// print_questions_number()
+
+
+
+
+// questions.forEach(i => {
+//     for(let j=0;j<i.answers.length;j++){
+//         const my_answers = document.createElement("button")
+//         my_answers.innerText = i.answers[j].text
+//         mainDiv.appendChild(my_answers)
+//     }        
+// })
+
+// console.log(questions)
